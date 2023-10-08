35757dc8f2111250644efe6fe7dd2576c75e7388
kevincalle3101 <kevinmendoza3101@hotmail.com>
2023-10-07 20:08:17 -0500

    navbar creation

--- components/HeroSection.tsx
+++ components/HeroSection.tsx
@@ -1,5 +1,8 @@
+"use client";
 import React from 'react';
 import Image from 'next/image';
+import { TypeAnimation } from 'react-type-animation';
+
 
 const HeroSection = () => {
     return (
@@ -8,14 +11,27 @@ const HeroSection = () => {
                 <div className='col-span-7 place-self-center text-center sm:text-left'>
                     <h1 className='text-4xl sm:text-5xl lg:text-6xl mb-4 font-extrabold' >
                         <span className='text-transparent bg-clip-text bg-gradient-to-r from-rose-300 to-rose-600' >Hello, I'm {" "}</span>
-                        Kevin Calle
+                        <br/>
+                        <TypeAnimation
+                            sequence={[
+                                'Kevin Calle',
+                                1000,
+                                'Frontend Developer',
+                                1000,
+                                'Backend Developer',
+                                1000
+                            ]}
+                            wrapper="span"
+                            speed={50}
+                            repeat={Infinity}
+                        />
                     </h1>
                     <p className='text-[#ADB7BE] mb-6 text-base sm:text-lg lg:text-xl'>Full Stack Web Developer</p>
                     <div>
                         <button className='px-6 w-full sm:w-fit py-3 rounded-full mr-4 bg-gradient-to-br from-rose-300 via-rose-500 to-rose-600 hover:bg-slate-200 text-white'>Hire Me</button>
-                        <button className='px-1 w-full sm:w-fit py-1 rounded-full bg-gradient-to-br from-rose-300 via-rose-500 to-rose-600 hover:bg-slate-800 text-white border border-white mt-3'>
+                        <button className='px-1 w-full sm:w-fit py-1 rounded-full bg-gradient-to-br from-rose-300 via-rose-500 to-rose-600 hover:bg-slate-800 text-white border border-none mt-3'>
                             <span className='block bg-[#121212] hover:bg-slate-800 rounded-full px-5 py-2'>Download CV</span>
-                            </button>
+                        </button>
                     </div>
                 </div>
                 <div className='col-span-5 place-self-center mt-4 lg:mt-0' >
---
+++ components/Navbar.tsx
@@ -0,0 +1,47 @@
+import React from 'react'
+import Link from 'next/link'
+import NavLink from './NavLink'
+
+interface NavLink {
+    title: string;
+    path: string;
+}
+
+const navLinks: NavLink[] = [
+    {
+        title: "About",
+        path: "#about"
+    },
+    {
+        title: "Projects",
+        path: "#projects"
+    },
+    {
+        title: "Contact",
+        path: "#contact"
+    }
+]
+
+const Navbar = () => {
+    return (
+        <nav>
+            <div className='flex flex-wrap items-center justify-between mx-auto p-8'>
+                <Link href={"/"} className='text-5xl text-white font-semibold'>KC</Link>
+                <div className='menu hidden md:block md:w-auto' id="navbar">
+                    <ul>
+                        {
+                            navLinks.map((link, index) => (
+                                <li key={index}>
+                                    <NavLink href={link.path} title={link.title} />
+                                </li>
+                            )
+                            )
+                        }
+                    </ul>
+                </div>
+            </div>
+        </nav>
+    )
+}
+
+export default Navbar
