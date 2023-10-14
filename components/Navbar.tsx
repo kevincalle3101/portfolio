383f5e52eb867d805f7d1f55db140f3fe502ae77
kevincalle3101 <kevinmendoza3101@hotmail.com>
2023-10-13 23:15:33 -0500

    logo change

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -30,7 +30,9 @@ const Navbar = () => {
     return (
         <nav className='fixed top-0 left-0 right-0 z-10 bg-[#121212] bg-opacity-100'>
             <div className='flex flex-wrap items-center justify-between mx-auto px-4 py-2'>
-                <Link href={"/"} className='text-3xl md:text-5xl text-white font-semibold'>KC</Link>
+                <Link href={"/"} className='text-3xl md:text-5xl text-white font-semibold'>
+                    <img src='/images/LogoKC.png' alt="logo" className='w-12 h-10' />
+                </Link>
                 <div className='mobile-menu block md:hidden'>{
                     !navbarOpen ? (
                         <button className='flex items-center px-3 py-2 border rounded border-slate-200 text-slate-200 hover:text-white hover:border-white'>
