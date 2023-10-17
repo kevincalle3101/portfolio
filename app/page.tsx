ad172043e808aaf9b7283bd28c83abb0bf8bb150
kevincalle3101 <kevinmendoza3101@hotmail.com>
2023-10-16 23:28:20 -0500

    changesWords

--- app/page.tsx
+++ app/page.tsx
@@ -5,7 +5,7 @@ export default function Home() {
   return (
     <main className="flex min-h-screen flex-col">
       <Navbar />
-      <div className="container mt-24 mx-auto px-12 py-4">
+      <div className="container mt-24 mx-auto px-8 py-4">
         <HeroSection />
       </div>
     </main>
--- components/HeroSection.tsx
+++ components/HeroSection.tsx
@@ -7,10 +7,10 @@ import { TypeAnimation } from 'react-type-animation';
 const HeroSection = () => {
 
     return (
-        <section>
-            <div className='grid items-center grid-cols-1 sm:grid-cols-10 gap-y-8' >
-                <div className='col-span-6 ml-0 text-center sm:text-left'>
-                    <h1 className='text-4xl sm:text-5xl lg:text-6xl mb-2 font-extrabold' >
+        <section className='lg:py-16'>
+            <div className='grid items-center grid-cols-1 sm:grid-cols-12 gap-y-8' >
+                <div className='col-span-8 ml-0 text-center sm:text-left sm:justify-self-start'>
+                    <h1 className='text-4xl sm:text-5xl lg:text-8xl mb-2 font-extrabold' >
                         <span className='text-transparent bg-clip-text bg-gradient-to-r from-rose-300 to-rose-600' >{`Hello, I'm`}</span>
                         <br />
                         <TypeAnimation
@@ -32,13 +32,13 @@ const HeroSection = () => {
                     <p className='text-[#ADB7BE] mb-5 text-base sm:text-lg sm:mt-2  lg:text-xl'>Full Stack Web Developer</p>
                     <div className='flex flex-arrow space-x-5 justify-center mb-5 mt-0 sm:justify-start'>
                         <a href="https://github.com/kevincalle3101" target="_blank" className='flex justify-center items-center transform scale-100 hover:scale-110 transition-transform duration-300'>
-                            <img alt='github' src='/images/logoGithub.png' width={42} height={42}/>
+                            <img alt='github' src='/images/logoGithub.png' width={40} height={40}/>
                         </a>
                         <a href="https://www.linkedin.com/in/kevin-calle-53935b273/" target="_blank" className='flex  justify-center items-center transform scale-100 hover:scale-110 transition-transform duration-300'>
-                            <img  src='/images/logoLinkedln.png' alt='linkedin' width={42} height={42}/>
+                            <img  src='/images/logoLinkedln.png' alt='linkedin' width={40} height={40}/>
                         </a>
                         <a href="mailto:kevincalle3101@gmail.com"  target="_blank" className='flex justify-center items-center transform scale-100 hover:scale-110 transition-transform duration-300'>
-                            <img src='/images/logoMail.png' alt='gmail' width={42} height={42}/>
+                            <img src='/images/logoMail.png' alt='gmail' width={40} height={40}/>
                         </a>
                     </div>
                     <div className='items-center'>
@@ -49,7 +49,7 @@ const HeroSection = () => {
                         </a>
                     </div>
                 </div>
-                <div className='col-span-4 lg:mt-0 flex place-self-center' >
+                <div className='col-span-4 lg:mt-0 place-self-center sm:ml-auto'>
                     <div className='rounded-full overflow-hidden bg-[#181818] w-[250px] h-[250px] relative lg:w-[400px] lg:h-[400px]'>
                         <Image
                             src='/images/fotoPerfilPorfolioKevin.png'
