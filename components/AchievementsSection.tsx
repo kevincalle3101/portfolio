5db9c96af0dca658b26fe8dcd2ce10a4db5e795a
kevincalle3101 <kevinmendoza3101@hotmail.com>
2023-11-07 22:32:41 -0500

    newChanges

--- components/AchievementsSection.tsx
+++ components/AchievementsSection.tsx
@@ -1,35 +1,74 @@
-import React from 'react'
+"use client";
+import React from "react";
+import dynamic from "next/dynamic";
 
-const AchievementsList = [
+const AnimatedNumbers = dynamic(
+  () => {
+    return import("react-animated-numbers");
+  },
+  { ssr: false }
+);
+
+type Achievement = {
+    metric: string;
+    value: string;
+    postFix?: string;
+};
+
+const AchievementsList: Achievement[] = [
     {
         metric: "Projects",
-        value: "5+"
+        value: "5",
+        postFix: "+",
     },
     {
-        metric: "Users",
-        value: "10+"
+        metric: "certificates",
+        value: "5",
+        postFix: "+",
     },
     {
-        metric: "Awards",
-        value: "5+"
+        metric: "Year",
+        value: "1",
     },
-    {
-        metric: "Years",
-        value: "1+"
+        {
+        metric: "Skills",
+        value: "10",
+        postFix: "+",
     }
 ]
 
 const AchievementsSection = () => {
-    return (
-        <div className='py-8 px-4 xl:gap-16 sm:py-16 xl:px-16'>{
-            AchievementsList.map((achievement, index) => (
-                <div key={index} className='flex flex-col items-center justify-center mx-4'>
-                    <h2 className='text-white text-4xl font-bold'>{achievement.value}</h2>
-                    <p className='text-[#ADB7BE] text-base'>{achievement.metric}</p>
-                </div>
-            ))
-        }</div>
-    )
-}
+  return (
+    <div className="py-8 px-4 xl:gap-16 sm:py-16 xl:px-16">
+      <div className="sm:border-[#33353F] sm:border rounded-md py-8 px-16 flex flex-col sm:flex-row items-center justify-between">
+        {AchievementsList.map((achievement, index) => {
+          return (
+            <div
+              key={index}
+              className="flex flex-col items-center justify-center mx-4 my-4 sm:my-0"
+            >
+              <h2 className="text-white text-4xl font-bold flex flex-row">
+                <AnimatedNumbers
+                  includeComma
+                  animateToNumber={parseInt(achievement.value)}
+                  locale="en-US"
+                  configs={(_, index) => {
+                    return {
+                      mass: 1,
+                      friction: 100,
+                      tensions: 140 * (index + 1),
+                    };
+                  }}
+                />
+                {achievement.postFix}
+              </h2>
+              <p className="text-[#ADB7BE] text-base">{achievement.metric}</p>
+            </div>
+          );
+        })}
+      </div>
+    </div>
+  );
+};
 
-export default AchievementsSection
+export default AchievementsSection;
